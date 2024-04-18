59c7301201168ab34081a4b6d5d66a2497a05330
Baptiste Moureaux <batoch3@hotmail.fr>
2024-04-18 23:59:16 +0200

    add NUM_OF_LETTERS_IN_ANSWER constant

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { NUM_OF_GUESSES_ALLOWED, NUM_OF_LETTERS_IN_ANSWER } from '../../constants';
 import { checkGuess } from '../../game-helpers';
 
 function GuessResults({ guessResults, answer }) {
@@ -9,7 +9,7 @@ function GuessResults({ guessResults, answer }) {
       {guessResults.slice(0, NUM_OF_GUESSES_ALLOWED).map((guess) => (
         <p key={guess.id} className="guess">{checkGuess(guess.guess, answer).map(({ letter, status }) => (<span key={crypto.randomUUID()} className={`cell ${status}`}>{letter}</span>))}</p>
       ))}
-      {Array(emptyGuesses).fill(0).map((_, i) => <p key={crypto.randomUUID()} className="guess">{Array(5).fill(0).map(() => <span key={crypto.randomUUID()} className="cell"></span>)}</p>)}
+      {Array(emptyGuesses).fill(0).map((_, i) => <p key={crypto.randomUUID()} className="guess">{Array(NUM_OF_LETTERS_IN_ANSWER).fill(0).map(() => <span key={crypto.randomUUID()} className="cell"></span>)}</p>)}
     </div>
   )
 }
