18330e520707bd951eeb5a98f1e22bb60ff3cd02
Baptiste Moureaux <batoch3@hotmail.fr>
2024-04-18 23:05:14 +0200

    add cells to guesses

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function GuessResults({ guessResults }) {
+  const emptyGuesses = guessResults.length < 6 ? NUM_OF_GUESSES_ALLOWED - guessResults.length : 0;
+  console.log(Array(emptyGuesses).fill(0))
   return (
     <div className="guess-results">
-      {guessResults.map((guess) => (
-        <p key={guess.id} className="guess">{guess.guess}</p>
+      {guessResults.slice(0, NUM_OF_GUESSES_ALLOWED).map((guess) => (
+        <p key={guess.id} className="guess">{guess.guess.split("").map(letter => (<span className="cell">{letter}</span>))}</p>
       ))}
+      {Array(emptyGuesses).fill(0).map((_, i) => <p key={i} className="guess">{Array(5).fill(0).map(() => <span className="cell"></span>)}</p>)}
     </div>
   )
 }
-
 export default GuessResults;
